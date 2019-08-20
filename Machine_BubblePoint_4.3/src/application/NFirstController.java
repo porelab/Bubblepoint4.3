ee20f216f3e89c196b85a36b9110d11e41891827
Jayesh <Jayesh@My-Dell>
2019-08-20 11:18:13 +0530

    Portlist Popup Add

--- Machine_BubblePoint_4.3/src/application/NFirstController.java
+++ Machine_BubblePoint_4.3/src/application/NFirstController.java
@@ -54,7 +54,7 @@ public class NFirstController implements Initializable {
 
 	@FXML
 	Button livetest, report, btncloud, btnsetting, txtuname, qtest, btnscada,
-			btnrefresh, btnhelp,btnclose;
+			btnrefresh, btnhelp,btnclose,btnport;
 
 	@FXML
 	Rectangle recmain;
@@ -172,7 +172,6 @@ public class NFirstController implements Initializable {
 		DataStore.sc = new SerialCommunicator();
 
 		addShortCut();
-		lblconnection.setText("Not Connected");
 		DataStore.connect_hardware.addListener(new ChangeListener<Boolean>() {
 
 			@Override
@@ -184,11 +183,14 @@ public class NFirstController implements Initializable {
 				if (arg2) {
 
 					lblconnection.setText("Connected(" + DataStore.getCom() + ")");
+					btnport.setVisible(false);
 				} else {
 					lblconnection.setText("Not Connected");
+					btnport.setVisible(true);
 				}
 			}
 		});
+		
 		
 		connectHardware(DataStore.getCom());
 
@@ -282,6 +284,19 @@ public class NFirstController implements Initializable {
 	}
 
 	void setBtnClicks() {
+		
+		
+		btnport.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				
+				mydia = new MyDialoug(Main.mainstage, "/application/Portlistpopup.fxml");
+				mydia.showDialoug();
+				
+			}
+		});
 
 		btnrefresh.setOnAction(new EventHandler<ActionEvent>() {
 
